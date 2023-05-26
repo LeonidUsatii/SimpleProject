64897c2c8aae32cdb5fad261eda35377d56bc93f
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2023-05-26 11:06:06 +0200

    add class Test1

--- src/test/java/de/ait/services/UsersServiceImplTest.java
+++ src/test/java/de/ait/services/UsersServiceImplTest.java
@@ -1,14 +1,19 @@
 package de.ait.services;
 
+import de.ait.repositories.UsersRepository;
+import de.ait.repositories.UsersRepositoryListImpl;
+import de.ait.repositories.UsersRepositoryTextFileImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 class UsersServiceImplTest {
-
+    private UsersServiceImpl usersService;
     @BeforeEach
     void setUp() {
-    }
+        this.usersService = new UsersServiceImpl(new UsersRepositoryListImpl());
+        }
+
 
     @Test
     void getNames() {
