d2a332de1a3709957a09f8db9f4ad3471b13669e
leonidusatiy <62097425+leonidusatiy@users.noreply.github.com>
2023-05-26 11:00:04 +0200

    add class Test

---
+++ src/test/java/de/ait/services/UsersServiceImplTest.java
@@ -0,0 +1,32 @@
+package de.ait.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class UsersServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getNames() {
+    }
+
+    @Test
+    void getLastNameOfMostAging() {
+    }
+
+    @Test
+    void getAverageAgeOfUsers() {
+    }
+
+    @Test
+    void getAgeOfTallestPerson() {
+    }
+
+    @Test
+    void getNameAndSurnameOfShortestPerson() {
+    }
+}
